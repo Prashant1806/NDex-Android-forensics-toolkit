65961667fa3cd3ac1bda330b1d1ed96eec5588ee
Prashant Goel <56596612+Prashant1806@users.noreply.github.com>
2023-05-31 17:05:01 +0530

    Create ExampleUnitTest.java

---
+++ app/src/test/java/com/ndex.com/NDex/ExampleUnitTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(2, 4 - 2);
+    }
+
+    @Test
+    public void multiplication_isCorrect() {
+        assertEquals(8, 4 * 2);
+    }
+
+    @Test
+    public void division_isCorrect() {
+        assertEquals(2, 4 / 2);
+    }
+}
